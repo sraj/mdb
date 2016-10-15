6dedff65646b178f2c38f2910502d1359c7d8a0c
Suman Raj <sumanraj.v@gmail.com>
2016-10-15 01:10:22 -0700

    added find and findone

--- core.go
+++ core.go
@@ -2,22 +2,33 @@ package mdb
 
 import (
 	"errors"
+	_ "fmt"
 	"time"
 
 	"gopkg.in/mgo.v2"
 )
 
-type (
-	Config struct {
-		ConnectionURL string
-		Database      string
-	}
+type M map[string]interface{}
 
-	Connection struct {
-		Config  *Config
-		session *mgo.Session
-	}
-)
+type Config struct {
+	ConnectionURL string
+	Database      string
+}
+
+type Connection struct {
+	Config  *Config
+	session *mgo.Session
+}
+
+type Query struct {
+	Filter M
+	Fields M
+	Sort   string
+	Limit  int
+	Skip   int
+}
+
+var defaultLimit = 20
 
 func NewConnection(config *Config) (*Connection, error) {
 	conn := &Connection{
@@ -60,4 +71,47 @@ func (t *Connection) Close() {
 	if t.session != nil {
 		t.session.Close()
 	}
+}
+
+func (t *Connection) DropDatabase() error {
+	return t.session.DB(t.Config.Database).DropDatabase()
+}
+
+func (t *Connection) Collection(name string) *mgo.Collection {
+	return t.session.DB(t.Config.Database).C(name)
+}
+
+func (t *Connection) Insert(name string, docs ...interface{}) error {
+	return t.Collection(name).Insert(docs...)
+}
+
+func (t *Connection) processQuery(iq Query, mq *mgo.Query) *mgo.Query {
+
+	if iq.Fields != nil {
+		mq = mq.Select(iq.Fields)
+	}
+	if iq.Sort != "" {
+		mq = mq.Sort(iq.Sort)
+	}
+	if iq.Skip > 0 {
+		mq = mq.Skip(iq.Skip)
+	}
+	if iq.Limit == 0 {
+		iq.Limit = defaultLimit
+	}
+	mq = mq.Limit(iq.Limit)
+
+	return mq
+}
+
+func (t *Connection) Find(name string, query Query, result interface{}) error {
+	mq := t.Collection(name).Find(query.Filter)
+	mq = t.processQuery(query, mq)
+	return mq.All(result)
+}
+
+func (t *Connection) FindOne(name string, query Query, result interface{}) error {
+	mq := t.Collection(name).Find(query.Filter)
+	mq = t.processQuery(query, mq)
+	return mq.One(result)
 }
